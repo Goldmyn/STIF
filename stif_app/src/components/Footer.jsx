37a8c42c3703b4b3b7fcf3ccd3d08fa7a69b2277
Marius Nwokolo <mariusnwokolo24@gmail.com>
2022-09-04 18:27:42 -0400

    modified:   stif_app/src/components/Footer.jsx

--- stif_app/src/components/Footer.jsx
+++ stif_app/src/components/Footer.jsx
@@ -47,8 +47,27 @@ const Footer = () => (
       </div>
     </div>
 
-    <div>
-      
+    <div className="w-full flex justify-between
+      items-center md:flex-row flex-col pt-6 border-t-[1px]
+      border-t-[#3f3r45]">
+      <p className="font-poppins font-normal text-[18px]
+                      leading-[27px] text-white"
+      >
+        2022 STIF. All Rights Reserved.
+      </p>
+
+      <div className="flex flex-row md:mt-0 mt-6">
+        {socialMedia.map((social, index) => (
+          <img 
+            key={social.id}
+            src={social.icon}
+            alt={social.id}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer
+              ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`}
+          />
+        ))}
+
+      </div>
     </div>
   </section>
   )
